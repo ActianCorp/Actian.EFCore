061041334fb4199093a0cbbdd60ba9b23a85de3d
Marius Ianculovici <marius.ianculovici@actian.com>
2025-08-06 09:20:50 -0400

    II-18416 - Add Actian copyright notices in EFCore code

--- test/Actian.EFCore.FunctionalTests/Query/NorthwindMiscellaneousQueryActianTest.cs
+++ test/Actian.EFCore.FunctionalTests/Query/NorthwindMiscellaneousQueryActianTest.cs
@@ -1,3 +1,7 @@
+﻿// Copyright (c) 2024 Actian Corporation. All Rights Reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -4088,7 +4092,7 @@ FROM (
 """);
     }
 
-    [ActianTodo]
+    [ActianTodo] // .Distinct() must preceed .Skip()
     public override async Task OrderBy_coalesce_skip_take_distinct_take(bool async)
     {
         await base.OrderBy_coalesce_skip_take_distinct_take(async);
